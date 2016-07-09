ca1408b85063384647d14ffcbb29a6fb3c6a3230
DoomG <serg.ipatov.si@gmail.com>
2016-07-09 11:43:10 +0300

    first commit

---
+++ Card.cs
@@ -0,0 +1,34 @@
+﻿namespace MindoxTesting
+{
+    public class Card
+    {
+        public string Source { get; set; }
+
+        public string Destination { get; set; }
+
+        public override string ToString()
+        {
+            return Source + "->" + Destination;
+        }
+
+        public static Card[] GetMixedSamples()
+        {
+            return new[]
+            {
+                new Card {Source = "Melbourne", Destination = "Koln"},
+                new Card {Source = "Moscow", Destination = "Paris"},
+                new Card {Source = "Koln", Destination = "Moscow"}
+            };
+        }
+
+        public static Card[] GetEmptySamples()
+        {
+            return new Card[]{};
+        }
+
+        public static Card[] GetNullSample()
+        {
+            return null;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MindoxTesting
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var cards = Card.GetMixedSamples();
+            var sortedCards = GetSortedCards(cards);
+            foreach (var card in sortedCards)
+            {
+                Console.WriteLine(card.ToString());
+            }
+            Console.ReadKey();
+        }
+
+        public static IEnumerable<Card> GetSortedCards(Card[] cards)
+        {
+            var result = new List<Card>();
+            var sourceDictionary = new Dictionary<string, Card>();
+            var destinationDictionary = new Dictionary<string, Card>();
+
+            for (int i = 0; i < cards.Length; i++) // Filling dictionaries
+            {
+                var buffer = cards[i];
+                sourceDictionary.Add(buffer.Source,buffer);
+                destinationDictionary.Add(buffer.Destination, buffer);
+            }
+
+            for (int i = 0; i < cards.Length; i++) // Looking for the first element
+            {
+                var buffer = cards[i];
+                if (!destinationDictionary.ContainsKey(buffer.Source))
+                {
+                    result.Add(buffer);
+                    break;
+                }
+            }
+
+            var current = result[0];
+            while (result.Count != cards.Length) // Sorting array
+            {
+                current = sourceDictionary[current.Destination];
+                result.Add(current);
+            }
+
+            return result;
+        } 
+    }
+}
