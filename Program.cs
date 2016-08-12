bdc6a21cd3432068c32ca8255159445b343aef47
Test111 <serg.i@mail.ru>
2016-08-12 21:25:43 +0300

    Revert "test from master-test"
    This reverts commit ce640f70a3d77d466f3d16763b4dce0ac6a4294c.

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace MindoxTesting
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("FUCK IT ALL TWICE");
+            Console.WriteLine("FUCK IT ALL");
             var cards = CardSamples.GetMixedSamples();
             var sortedCards = GetSortedCards(cards);
             foreach (var card in sortedCards)
